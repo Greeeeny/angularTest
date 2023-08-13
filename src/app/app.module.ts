6759613ae2209a80e9b151884324e68a8dd7a52c
Yixuan Zhou <zyx2244@outlook.com>
2023-08-13 18:13:04 +0800

    create component 1 and 2 following lessons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { oneComponent } from './one';
+import { TwoComponent } from './two.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//声明
+    AppComponent,
+    oneComponent,
+    TwoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]//start component: AppComponent
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//start module : AppModule
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
