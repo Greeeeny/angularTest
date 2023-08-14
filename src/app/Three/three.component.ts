b382358c392dd09a0cbbd7ee7043b082faf6bd50
Yixuan Zhou <zyx2244@outlook.com>
2023-08-14 23:42:59 +0800

    update codes with lesson Augular-3.4 npm.

---
+++ src/app/Three/three.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+//h2默认加粗
+@Component({
+    selector: 'three-component',
+    templateUrl:'./three.component.html',
+    styleUrls:['./three.component.css']
+})
+export class ThreeComponent{
+
+}
