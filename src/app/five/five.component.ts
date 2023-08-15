353b5fe1ff027683f10c1ae50924b9b297c2e08b
Yixuan Zhou <zyx2244@outlook.com>
2023-08-15 23:43:27 +0800

    update notes on 8/15

--- src/app/five/five.component.ts
+++ src/app/five/five.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+//<p>里，title属性需鼠标悬停一段时间出现
 @Component({
   selector: 'app-five',
   templateUrl: './five.component.html',
   styleUrls: ['./five.component.scss']
 })
-export class FiveComponent implements OnInit {
+export class FiveComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+//MVVM： Model
+uname = 'test name' //可以不写也可以写‘；’
+uage = 20
+printUname() {
+  console.log(this.uname)
   }
 
 }
